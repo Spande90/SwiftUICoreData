10ba1f3558ae935ab17706224863ddc64067862d
Siddharth Pande <siddharth.v.pande@gmail.com>
2021-04-26 22:46:44 +0530

    Initial Commit

---
+++ SwiftUICoreData/SwiftUICoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoreDataApp.swift
+//  SwiftUICoreData
+//
+//  Created by Siddharth on 26/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
